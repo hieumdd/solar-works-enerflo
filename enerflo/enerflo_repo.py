4f95cf6214b5194c6bd56f3973436a3aa648ab45
HM <hieumdd@gmail.com>
2022-04-09 10:43:54 +0700

    feat: Switch to async for get

--- enerflo/enerflo_repo.py
+++ enerflo/enerflo_repo.py
@@ -1,31 +1,55 @@
-from typing import Any
+from typing import Any, Callable
 import os
+import math
+import asyncio
 
-import requests
+import httpx
 
 BASE_URL = "https://enerflo.io/api/v1"
 PAGE_SIZE = 500
 
 
-def get_client() -> requests.Session:
-    client = requests.Session()
-    client.headers.update({"api-key": os.getenv("ENERFLO_API_KEY", "")})
-    return client
+async def _get_one(
+    client: httpx.AsyncClient,
+    url: str,
+    callback_fn: Callable[[dict], Any],
+    page: int = 1,
+) -> dict[str, Any]:
+    r = await client.get(
+        url,
+        params={
+            "pageSize": PAGE_SIZE,
+            "page": page,
+        },
+    )
+    r.raise_for_status()
+    res = r.json()
+    return callback_fn(res)
 
 
 def get(endpoint: str):
-    def _get(client: requests.Session):
-        def __get(page: int = 1) -> list[dict[str, Any]]:
-            with client.get(f"{BASE_URL}/{endpoint}", params={
-                "pageSize": PAGE_SIZE,
-                "page": page,
-            }) as r:
-                r.raise_for_status()
-                res = r.json()
-            data = res["data"]
-            return data if not data else data + __get(page + 1)
+    def _get():
+        async def __get():
+            async with httpx.AsyncClient(
+                headers={"api-key": os.getenv("ENERFLO_API_KEY", "")},
+                timeout=None,
+            ) as client:
+                url = f"{BASE_URL}/{endpoint}"
+                count = await _get_one(client, url, lambda x: x["dataCount"])
+                pages = math.ceil(count / 500)
+                tasks = [
+                    asyncio.create_task(
+                        _get_one(
+                            client,
+                            url,
+                            lambda x: x["data"],
+                            page,
+                        )
+                    )
+                    for page in range(1, pages + 1)
+                ]
+                return await asyncio.gather(*tasks)
 
-        return __get
+        return [i for j in asyncio.run(__get()) for i in j]
 
-    with get_client() as client:
-        return _get(client)
+    return _get
