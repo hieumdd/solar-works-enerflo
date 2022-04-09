3fd1de08a864faba123e4349f23d68cbabf38857
HM <hieumdd@gmail.com>
2022-04-09 10:23:24 +0700

    feat: Add pagination

--- enerflo/enerflo_repo.py
+++ enerflo/enerflo_repo.py
@@ -4,6 +4,7 @@ import os
 import requests
 
 BASE_URL = "https://enerflo.io/api/v1"
+PAGE_SIZE = 500
 
 
 def get_client() -> requests.Session:
@@ -14,10 +15,15 @@ def get_client() -> requests.Session:
 
 def get(endpoint: str):
     def _get(client: requests.Session):
-        def __get() -> list[dict[str, Any]]:
-            with client.get(f"{BASE_URL}/{endpoint}") as r:
+        def __get(page: int = 1) -> list[dict[str, Any]]:
+            with client.get(f"{BASE_URL}/{endpoint}", params={
+                "pageSize": PAGE_SIZE,
+                "page": page,
+            }) as r:
+                r.raise_for_status()
                 res = r.json()
-            return res["data"]
+            data = res["data"]
+            return data if not data else data + __get(page + 1)
 
         return __get
 
